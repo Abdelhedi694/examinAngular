5728f4c7534e82bd9e67829ca834fc44a4ddf0f2
Abdelhedi Rafaï <abdel.hedi69400@gmail.com>
2021-12-10 11:17:33 +0100

    exercice 4

--- examinStatistique/src/app/app.component.ts
+++ examinStatistique/src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { Statistique } from './models/statistique';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'examinStatistique';
+  covidFrance: Statistique = new Statistique(
+    'sfhkjhdflkv95995',
+    'covid France',
+    '8 Millions'
+  );
+  covidDecesFrance: Statistique = new Statistique(
+    'jhjljcsmskj844455',
+    'Décès covid France',
+    '120 000'
+  );
 }
